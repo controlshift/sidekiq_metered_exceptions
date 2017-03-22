df7d20b213fc9e9e1f8067eabe6211dd49df6c92
anero <anero79@gmail.com>
2017-03-22 15:06:01 -0300

    Fixed key on context and added additional logging for debugging

--- lib/metered_raven_error_handler.rb
+++ lib/metered_raven_error_handler.rb
@@ -4,9 +4,13 @@ require 'raven/integrations/sidekiq'
 module SidekiqMeteredExceptions
   class MeteredRavenErrorHandler < ::Raven::Sidekiq
     def call(ex, context)
-      retry_count = (context['retry_count'] || (context['job'] && context['job']['retry_count']))
+      ::Rails.logger.debug("MeteredRavenErrorHandler -- Error on Sidekiq job. Exception: #{ex.inspect} - Context: #{context.inspect}")
+
+      retry_count = (context['retry_count'] || (context[:job] && context[:job]['retry_count'])).try(:to_i)
 
       if retry_count.nil? || retry_count > 1
+        ::Rails.logger.debug("MeteredRavenErrorHandler -- Current retry count: #{retry_count}. Notifying upstream...")
+
         super(ex, context)
       end
     end
